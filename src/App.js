728d63a96f2041b708dc03e55fface698eaf748d
Shivam Tayal <92094383+tayalshivu1@users.noreply.github.com>
2023-07-19 03:44:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,31 +6,31 @@ function App() {
       id: 1,
       expenseDate: new Date(2023, 10, 23),
       title: "Car Insurance",
-      price: "Rs 5000"
+      price: "Rs 5000",
     },
     {
       id: 2,
       expenseDate: new Date(2022, 1, 12),
       title: "Bike Insurance",
-      price: "Rs 2000"
+      price: "Rs 2000",
     },
     {
       id: 3,
       expenseDate: new Date(2022, 4, 27),
       title: "Health Insurance",
-      price: "Rs 10000"
+      price: "Rs 10000",
     },
     {
       id: 4,
       expenseDate: new Date(2023, 3, 30),
       title: "Restaurant visit",
-      price: "Rs 3000"
-    }
+      price: "Rs 3000",
+    },
   ];
 
   return (
     <div>
-      <h1>Expense Tracker</h1>
+      <h1>Expense Tracker App</h1>
       <Expenses expenses={expenses} />
     </div>
   );
